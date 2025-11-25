c1d6d419f86eb779c7c49cff2a944d617ab7375c
tle-dref <thomas.ledref@gmail.com>
2025-11-25 05:43:14 +0100

    add ELF parsing, file checking and Woody crafting

--- inc/woody.h
+++ inc/woody.h
@@ -1,10 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+#pragma once
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <sys/mman.h>
+# include <elf.h>
 
-
+typedef struct s_elf {
+	void   *map;
+	size_t  size;
+	int     fd;
+}   t_elf;
